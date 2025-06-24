3fdf75ec0e5d36e9c0b404bf44caeaed74f629d7
John Hamilton <jhamilton@buckeyeit.com>
2025-06-23 20:38:21 -0400

    Require login for all main views, add logout, and redirect root to login

--- portal/views.py
+++ portal/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, 'portal/index.html')
 
 def login_view(request):
     return render(request, 'portal/login.html')
 
+@login_required
 def password_view(request):
     return render(request, 'portal/password.html')
+
+def root_redirect(request):
+    return redirect('login')
