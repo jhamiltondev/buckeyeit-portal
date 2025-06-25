60a970703e1f3d2e0fc6e180bc2050991a447174
John Hamilton <jhamilton@buckeyeit.com>
2025-06-25 01:26:15 -0400

    Remove duplicate login/logout paths from main urls.py to avoid conflicts with portal app

--- buckeyeit_portal/urls.py
+++ buckeyeit_portal/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('portal.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
 ]
