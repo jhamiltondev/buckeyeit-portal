a97aa6ea4c5753aa3aa5c8547ca5688dc57d6013
John Hamilton <jhamilton@buckeyeit.com>
2025-06-25 07:53:17 -0400

    Add standard and Azure-compatible logout URLs to portal app

--- portal/urls.py
+++ portal/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'portal'
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', views.login_view, name='login'),
     path('password/', views.password_view, name='password'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('.auth/logout', auth_views.LogoutView.as_view(next_page='/login/')),
 ] 
