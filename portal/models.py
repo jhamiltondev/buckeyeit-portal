56faf988f340214e0b2eaa2866a90c1226953ae5
John Hamilton <jhamilton@buckeyeit.com>
2025-06-25 15:05:02 -0400

    Add VIP boolean field to Tenant model for admin control of automated features

--- portal/models.py
+++ portal/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Tenant(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    vip = models.BooleanField(default=False, help_text="Check if this tenant is VIP and should see automated features.")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
